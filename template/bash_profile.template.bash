d8fa8ec3a382b0ed0325e1e2a7d204b48084af7b
Carlo Zottmann <carlo@municode.de>
2012-12-12 11:24:03 +0100

    [NEW] Added support for npm modules.

--- template/bash_profile.template.bash
+++ template/bash_profile.template.bash
@@ -3,8 +3,8 @@
 # Load RVM, if you are using it
 [[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
 
-# Add rvm gems and nginx to the path
-export PATH=$PATH:~/.gem/ruby/1.8/bin:/opt/nginx/sbin
+# Add rvm gems, nginx and npm modules to the path
+export PATH=$PATH:~/.gem/ruby/1.8/bin:/opt/nginx/sbin:./node_modules/.bin
 
 # Path to the bash it configuration
 export BASH_IT=$HOME/.bash_it
