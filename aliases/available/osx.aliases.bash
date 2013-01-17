527ac4a99d9af4f76261475f2aac372886e9f4ea
Carlo Zottmann <carlo@municode.de>
2013-01-17 13:07:17 +0100

    [NEW] Added speed_up_terminal (ASL file cleanup, .hushlogin touching).

--- aliases/available/osx.aliases.bash
+++ aliases/available/osx.aliases.bash
@@ -2,19 +2,19 @@ cite 'about-alias'
 about-alias 'osx-specific aliases'
 
 # Desktop Programs
-alias preview="open -a '$PREVIEW'"
-alias xcode="open -a '/Developer/Applications/Xcode.app'"
-alias filemerge="open -a '/Developer/Applications/Utilities/FileMerge.app'"
-alias safari="open -a safari"
-alias firefox="open -a firefox"
 alias chrome="open -a google\ chrome"
 alias f='open -a Finder '
+alias filemerge="open -a '/Developer/Applications/Utilities/FileMerge.app'"
+alias firefox="open -a firefox"
+alias preview="open -a '$PREVIEW'"
+alias safari="open -a safari"
 alias textedit='open -a TextEdit'
+alias xcode="open -a '/Developer/Applications/Xcode.app'"
 
 alias dedrm_epub="aineptepub.py ~/Dropbox/Documents/adeptkey.der"
 alias dnsflush="dscacheutil -flushcache; sudo killall -HUP mDNSResponder"
-alias rebuild_context_menu="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user"
 alias ls='ls -alFG'
+alias rebuild_context_menu="sudo /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
 alias v="vagrant"
 
 # enable bash 4 recursive globbing
@@ -24,4 +24,5 @@ shopt -s globstar
 # if necessary, update the values of LINES and COLUMNS
 shopt -s checkwinsize
 
-
+# Clean up ASL files to make iTerm/Terminal startup speedy again
+alias speed_up_terminal="touch ~/.hushlogin; sudo rm -rf /private/var/log/asl/*.asl"
