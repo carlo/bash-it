71408518b710e11eb13fefdd38764b925a909b56
Carlo Zottmann <carlo@municode.de>
2013-03-19 22:33:54 +0100

    [NEW] Added chruby plugin.

---
+++ plugins/available/chruby.plugin.bash
@@ -0,0 +1,6 @@
+cite about-plugin
+about-plugin 'load chruby, if you are using it'
+
+RUBIES=(~/.rbfu/rubies/*)
+source /usr/local/opt/chruby/share/chruby/chruby.sh
+source /usr/local/opt/chruby/share/chruby/auto.sh
