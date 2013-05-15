54c1d4b06bc546da81f0696f955789e22f4309e4
Carlo Zottmann <carlo@municode.de>
2013-05-15 22:40:52 +0200

    [DEL] Removed RUBIES as chruby doesn't care about it anymore.

--- plugins/available/chruby.plugin.bash
+++ plugins/available/chruby.plugin.bash
@@ -1,6 +1,5 @@
 cite about-plugin
 about-plugin 'load chruby, if you are using it'
 
-RUBIES=(~/.rbfu/rubies/*)
 source /usr/local/opt/chruby/share/chruby/chruby.sh
 source /usr/local/opt/chruby/share/chruby/auto.sh
